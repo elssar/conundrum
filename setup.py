47021ae57ba1c6b9124a16bb9c95590ade6be56c
Akshay Bist <reddevil.akshay@gmail.com>
2013-02-14 05:41:15 +0530

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from sys import path
 
 setup(name= 'conundrum',
     version= '0.1.0',
     author= 'elssar',
     author_email= 'elssar@altrawcode.com',
-    packages= find_packages(),
+    py_modules= ['conundrum'],
     url= 'https://github.com/elssar/conundrum',
     licence= 'MIT',
     description= 'A framework agnostic blog generator.',
-    long_description= open('README.md').read(),
+    long_description= open(path[0]+'/README.md').read(),
     install_requires= [
-        'PyYAML >= 3.10',
+        'PyYAML >= 3.0.9',
         'Markdown >= 2.2.0',
-        'requests >= 1.0.4'
+        'requests >= 1.0.4',
         ],
 )
